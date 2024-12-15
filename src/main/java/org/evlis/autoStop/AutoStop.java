1dc89b63d3c47f58d4a11c84cf8e70894ee43c76
Peter Pan <peter@never.lan>
2024-12-15 16:03:46 -0500

    move to listener method

--- src/main/java/org/evlis/autoStop/AutoStop.java
+++ src/main/java/org/evlis/autoStop/AutoStop.java
@@ -1,20 +1,23 @@
 package org.evlis.autoStop;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class AutoStop extends JavaPlugin {
+public final class AutoStop extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
 
-        // Schedule task for 3-seconds post-startup
-        Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){
-            @Override
-            public void run(){
-                // Gracefully stop the server with a 10 second delay.
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "stop");
-            }
+    @EventHandler
+    public void onServerLoad(ServerLoadEvent event) {
+        // Schedule the server to stop 3 seconds (20 ticks per second) after server load is completed
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "stop");
         }, 60L);
     }
 
